f95dcf48bb4046e52f8b4748febd2bfd69eeefc7
Warpten <vertozor@gmail.com>
2013-01-28 06:53:56 +0100

    Saferized a bit the uploading page. The "Build enforcing" part is not used *for now*.

--- upload.php
+++ upload.php
@@ -1,6 +1,5 @@
 <?php
-$buildVersions = array(0 => "Zero",
-                       5875 => "1.12.1 5875",
+$buildVersions = array(5875 => "1.12.1 5875",
                        6180 => "2.0.1 6180", 6299 => "2.0.3 6299", 6337 => "2.0.6 6337",
                        6692 => "2.1.0 6692", 6739 => "2.1.1 6739", 6803 => "2.1.2 6803", 6898 => "2.1.3 6898",
                        7272 => "2.2.0 7272", 7318 => "2.2.2 7318", 7359 => "2.2.3 7359",
@@ -21,10 +20,16 @@ require_once('includes/header.php');
     <fieldset>
         <legend>Sniff Data Upload</legend>
         <textarea name="sniffData" style="width: 100%; height: 150px"></textarea>
-        <br />
-        If the dump is quite heavy, upload the file here. (Up to <b>10Mbs</b>)<br />
+        <br /><br />
+        If the sniff data dump is quite heavy, upload the .SQL file here. (Up to <b>10Mbs</b>)<br />
         <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
         <input type="file" name="fileData" />
+        <br />
+        <!--Overwrite build in SQL: <input type="radio" name="overwriteBuild" value="1" /> Yes <input type="radio" name="overwriteBuild" value="0" checked /> No<br />
+        <div id="buildEnforcer" style="display: none"><?php
+            foreach ($buildVersions as $val => &$build)
+                echo "<input type=\"radio\" name=\"enforcedBuildId\" value=\"{$val}\"> {$build} <br />";
+        ?></div>-->
         <input type="submit" name="submit" class="submit" value="Submit" />
     </fieldset>
 </form>
@@ -32,42 +37,79 @@ require_once('includes/header.php');
 <?php
 if (isset($_POST['submit'])) {
     $errors = array();
-    if (!empty($_POST['sniffData'])) {
-        $mysqlCon->multi_query($_POST['sniffData']);
-    } else if (!empty($_FILES["fileData"])) {
+    if (!empty($_POST['sniffData']))
+        injectSQL($_POST['sniffData']);
+    else if (!empty($_FILES["fileData"])) {
         $data = &$_FILES['fileData'];
         $allowedFile = false;
+
         $pathInfo = pathinfo($data['name']);
         if ($data['type'] === "application/octet-stream") // SQL files have that type, so we should extract extention from the pathinfo
             if ($pathInfo['extension'] === "sql")
                 if ($data['size'] < 10 * 1024 * 1024)
                     if (!$data['error'])
                         $allowedFile = true;
+        unset($pathInfo);
+
         if (!$allowedFile) {
             switch ($data['error']) {
                 case 1: // UPLOAD_ERR_INI_SIZE
                 case 2: // UPLOAD_ERR_FORM_SIZE
-                    $errors[] = "The file's size exceeds the limit.";
+                    $errors[] = "The SQL dump's size exceeds the limit.";
                     break;
                 case 3: // UPLOAD_ERR_PARTIAL
-                    $errors[] = "The file has only been partially transferred.";
+                    $errors[] = "The SQL dump has only been partially transferred.";
                     break;
                 case 4: // UPLOAD_ERR_NO_FILE:
-                    $errors[] = "No file has been uploaded.";
+                    // $errors[] = "No file has been uploaded."; // Debug only
                     break;
             }
+        }
+        else
+            injectSQL(file_get_contents($data['tmp_name']));
+    }
+    
+    if (!empty($errors)) {
+        echo "<ul>";
+        foreach ($errors as &$error)
+            echo "<li>" . $error. "</li>";
+        echo "</ul>";
+    }
+}
+
+function injectSQL($commandBlock) {
+    $commandLines  = array_map("trim", explode(chr(13), $commandBlock));
+    $lineIndex     = 0;
+    $lineCount     = count($commandLines);
+    $insertCommand = "";
+    while ($lineIndex < $lineCount) {
+        $line = &$commandLines[$lineIndex];
+        ++$lineIndex;
+
+        if (empty($line))
+            continue;
+
+        if ($line[0] === "(") { // New record - Inject it, if we have a valid INSERT header
+            if ($insertCommand !== "")
+                $mysqlConn->query($insertCommand . substr($line, -1)); // Remove the coma or semicolon.
         } else {
-            // Read the file
-            $commandBlock = file_get_contents($data['tmp_name']);
-            $mysqlCon->multi_query($commandBlock);
+            $lineTokens = explode(" ", $line);
+            $isIgnore = ($lineTokens[1] === "IGNORE");
+            if ($lineTokens[0] === "INSERT" && (($isIgnore && $lineTokens[2] === "INTO") || (!$isIgnore && $lineTokens[1] === "INTO"))) {
+                $insertCommand = ""; // We ensure nothing will get injected until we meet another INSERT query
+                if (in_array(strtolower($lineTokens[$isIgnore ? 3 : 2]), array("sniffdata", "objectnames"))) // We're all set, assume it is a valid INSERT query
+                    $insertCommand = $line;
+            }
         }
     }
 }
 ?>
 <script src="./includes/jquery.js"></script>
-<script>
+<script type="text/javascript">
 $(function() {
-
+    $("input[name='overwriteBuild']").click(function() {
+        $("#buildEnforcer").css("display", $(this).val() == 1 ? "block" : "none");
+    });
 });
 </script>
 <?php
